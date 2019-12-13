8822cb588543812a1739afa67256e49be859eb68
juan-eduar <chirinoj180918@gmail.com>
2019-12-12 20:16:08 -0400

    add-img

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -57,17 +57,46 @@ $('.slider-for').slick({
   asNavFor: '.slider-nav'
 });
 $('.slider-nav').slick({
-  slidesToShow: 3,
+  slidesToShow: 2,
   slidesToScroll: 1,
   asNavFor: '.slider-for',
   autoplay: true,
   autoplaySpeed: 4000,
-  dots: true,
+  dots: false,
   centerMode: true,
-  focusOnSelect: true
-  
+  focusOnSelect: true,
+  responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+    ]
 
-});
+
+
+  });
 $('.main-banner-content__opciones').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
